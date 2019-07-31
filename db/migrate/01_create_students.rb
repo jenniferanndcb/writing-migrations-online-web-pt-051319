e89d7c01b0207fff01187f5b2000ab34886a3b8a
Jen <jenniferanndcb@gmail.com>
2019-07-31 13:14:12 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,9 +3,8 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change 
     create_table :students do 
       |t|
-      
-      t.string 
-    
+      t.string :name
+    end 
   end 
   
 end
